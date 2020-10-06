987c8b6d4fb50daa2b1fee26b02955cf44f7ea5d
Samuel Nogueira <samuelsampaiox@gmail.com>
2020-10-06 15:43:04 -0300

    feature: os campos nao pode ser vazios

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-// Adicionamos nosso código dentro de uma função anônima para evitar
-// variáveis globais.
-(function() {
+(function () {
   // Elemento principal do formulário
-  const contactForm = document.getElementById('contact');
+  const contactForm = document.getElementById('contato_form');
 
   // Campos do formulário
   const nameInput = document.getElementById('name');
@@ -12,19 +10,33 @@
   // "addEventListener" instrui o navegador a executar uma função sempre
   // que um evento específico acontece. Nesse caso, a função abaixo será
   // executada toda vez que o botão de envio for pressionado.
-  contactForm.addEventListener('submit', function() {
+  contactForm.addEventListener('submit', function (event) {
+    event.preventDefault();
     // A propriedade "value" contém o valor atual de um input ou textarea
     const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
 
     // A propriedade "length" contém o tamanho (em caracteres) de uma
     // uma string. Toda string contém essa propriedade.
-    const nameLength = nameValue.length;
+    // const nameLength = nameValue.length;
 
     // "window" é um objeto global que representa a janela (ou aba) do
     // navegador que está executando o código do seu site. O método
     // "alert" simplesmente mostra um aviso para o usuário contendo a
     // mensagem provida.
-    window.alert(`Botão enviar clicado! Conteúdo do campo nome: ${nameValue} (${nameLength} caracteres)`);
+
+    if (nameValue.trim() === '') {
+      window.alert('O campo "Nome" não pode ficar vazio');
+    } else if (emailValue.trim() === '') {
+      window.alert('O campo "Email" cu não pode ficar vazio');
+    } else if (messageValue.trim() === '') {
+      window.alert('O campo "Mensagem" não pode ficar vazio');
+    } else {
+      window.alert('Mensagem enviada com sucesso!');
+    }
+
+    // alert('O campo email não pode ficar vazio');
 
     // Altere e complete essa função para validar os campos do formulário
     // de acordo com as especificações do teste. Boa sorte!
